286fd31f753626c1f0fa7a0a38e539c52a80300e
Tony Garcia <tony.garcia.ex@consalud.cl>
2019-11-08 20:00:05 -0300

    (add) se agrega mas texto en readme

--- README.md
+++ README.md
@@ -2,16 +2,22 @@
  Api construida para exponer de forma rest que consulta en redis coordenas(latitud, longitud) de los paises y luego consulta el servico de clima api rest [developer.forecast](https://developer.forecast.io/)
  y [darksky.net](https://darksky.net/dev) que son servidores de clima por regiones ubicadas por latitud y longitud
 
-## Para Iniciar
+### Pre-requisitos 📋
 
-### Starting Redis
+## Starting Redis
+
+```
 ... open shell linux
+```
 
 # redis-server
 
 En caso de no tener servidor redis
+```
+$ cd seidor-test-api/redis-server && docker-compose up --build
+```
 
-$ cmd seidor-test-api/redis-server && docker-compose up --build
+Redis quedara corriendo por defecto en puerto 6379
 
 ## Development mode
 
@@ -19,11 +25,15 @@ Run `npm run start-dev` for a dev server. Navigate to `http://localhost:3000/`. 
 
 # Usage
 
+```
 npm i
 npm run start
-
+```
 
 # Reset datos cargado en redis
+
+```
 npm i
 npm run start
 Navigate to `http://localhost:3000/resetdataredis`. Este servicio reseteara datos cargados en redis.
+```
--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
 import express from "express"
 import { health, weather } from "./routes"
 import redis from "redis"
-
+import latitudeData from "./constants"
 // create and connect redis client to local instance.
-const client = redis.createClient(6379)
+const client = redis.createClient(process.env.REDIS_PORT || 6379)
  
 // echo redis errors to the console
 client.on('error', (err) => {
@@ -17,35 +17,8 @@ client.on('error', (err) => {
     if (data) {
             console.log({ source: 'cache', data: JSON.parse(data) })
     } else { 
-       // Initial data load
-        let latitudeData = [
-          { 
-            name: "Santiago (CL)",
-            coord: "-33.4489, -70.6693"
-          },
-          {
-            name: "Zurich (CH)",
-            coord: "47.36667, 8.55"
-          },
-          {
-            name: "Auckland (NZ)",
-            coord: "-36.86667, 174.76667"
-          },  
-          {
-            name: "Sydney (AU)",
-            coord: "-33.86785, 151.20732"
-          },
-          {
-            name: "Londres (UK),",
-            coord: "51.509865, -0.118092"
-          },
-          {
-            name: "Georgia, USA",
-            coord: "33.247875, -83.441162"
-          }
-        ]
-        
-        client.setex(dataRedisKey, 3600, JSON.stringify(latitudeData))      
+      console.log(latitudeData)
+      client.setex(dataRedisKey, 3600, JSON.stringify(latitudeData))      
     }
 })
 
